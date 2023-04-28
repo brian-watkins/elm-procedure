2af09b7c4f465d24e93c1a01652fde343748c640
Brian Watkins <brian.watkins@gmail.com>
2023-04-28 07:09:27 -0400

    Use playwright to start sample app for tests

--- playwright.config.js
+++ playwright.config.js
@@ -70,10 +70,10 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start:integration',
+    url: 'http://127.0.0.1:9732',
+    reuseExistingServer: true,
+  },
 });
 
